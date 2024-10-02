e3df4c01fefd98d71a04bc4daae8753bade6c200
dmiweb <dimbons@mail.ru>
2024-10-02 19:14:07 +0300

    upd files

--- src/js/Polling.js
+++ src/js/Polling.js
@@ -1,11 +1,11 @@
 import handlerLongText from "./handlerLongText";
 import converterTimestamp from "./converterTimestamp";
-import { switchMap, interval, map, catchError, EMPTY } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { switchMap, interval, map, catchError, EMPTY } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
 export default class Polling {
   constructor() {
-    this.url = 'https://polling-backend-lac.vercel.app/messages/unread';
+    this.url = "https://polling-backend-lac.vercel.app/messages/unread";
   }
 
   static markupMessage(id, email, title, date) {
@@ -19,34 +19,35 @@ export default class Polling {
   }
 
   renderMessage() {
-    const messagesContainer = document.querySelector('.incoming-messages');
+    const messagesContainer = document.querySelector(".incoming-messages");
     const stream$ = this.requestMessage();
 
     stream$.subscribe(({ response }) => {
       const { messages } = response;
 
-      messages.forEach(message => {
+      messages.forEach((message) => {
         const id = message.id;
         const email = message.from;
         const title = handlerLongText(message.subject);
         const date = converterTimestamp(message.received);
 
-        messagesContainer.insertAdjacentHTML("afterBegin", Polling.markupMessage(id, email, title, date));
+        messagesContainer.insertAdjacentHTML(
+          "afterBegin",
+          Polling.markupMessage(id, email, title, date)
+        );
       });
-    })
+    });
   }
 
   requestMessage() {
-    const stream$ = interval(3000)
-      .pipe(
-        switchMap(() => {
-          return ajax(this.url)
-            .pipe(
-              map(response => response),
-              catchError(() => EMPTY)
-            )
-        })
-      );
+    const stream$ = interval(3000).pipe(
+      switchMap(() => {
+        return ajax(this.url).pipe(
+          map((response) => response),
+          catchError(() => EMPTY)
+        );
+      })
+    );
 
     return stream$;
   }
